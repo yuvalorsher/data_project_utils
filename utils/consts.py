58df0a618bab1ee65b00cc20dd8a3707a54b3647
corticodes <46958640+corticodes@users.noreply.github.com>
2024-11-10 17:36:19 +0200

    Add current status of project

---
+++ utils/consts.py
@@ -0,0 +1,16 @@
+# Conversion for frequency shorthand to x-axis title
+FREQ_SHORT_TO_FULL = dict(
+            D = 'Day',
+            W = 'Week',
+            M = 'Month',
+            Y = 'Year',
+            Q = 'Quarter',
+)
+
+# NaN imputation dictionary column names
+VALUES_COL_KEY = 'values_col'
+GROUPBY_COL_KEY = 'groupby_cols'
+
+LABEL_ENCODED_POSTFIX = '_label_encoded'
+TARGET_ENCODED_POSTFIX = '_target_encoded'
+
---
+++ utils/general.py
@@ -0,0 +1,7 @@
+def copy_if_not_inplace(df: pd.DataFrame, inplace: bool) -> pd.DataFrame:
+    df = df if inplace else df.copy()
+    return df
+
+def make_list_if_single_item(x) -> list:
+    x = x if isinstance(x, list) else [x]
+    return x
